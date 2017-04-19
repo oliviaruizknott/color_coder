effe30e66b93e192a6455966c808c52d01b144d8
K9Advantix <george53@bu.edu>
2017-04-19 16:29:26 -0400

    devise stuff

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
