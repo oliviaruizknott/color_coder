b2eb766c2ed9b9c70f5c58189081a7f8aba27f38
olivia@ruiz-knott.com <olivia@ruiz-knott.com>
2017-04-26 16:21:48 -0400

    Tests renamed and admin_features broken into smaller tests

---
+++ spec/features/20_admin_views_all_users_spec.rb
@@ -0,0 +1,96 @@
+require "rails_helper"
+require_relative "../factories/user_factory"
+require_relative "../factories/color_factory"
+require_relative "../factories/review_factory"
+
+feature "admins can see a list of users" do
+  scenario "from a link on the home page" do
+    user = FactoryGirl.create(:user)
+    color = FactoryGirl.create(:color, user: user)
+    FactoryGirl.create(:review, user: user, color: color)
+    FactoryGirl.create(:user, first_name: "Jane")
+    FactoryGirl.create(:user, first_name: "Jim")
+    FactoryGirl.create(:user, first_name: "James")
+    FactoryGirl.create(:user, first_name: "Janet")
+    admin = FactoryGirl.create(:user, role: 'admin')
+
+    visit root_path
+    click_link "Sign In"
+
+    fill_in 'Email', with: admin.email
+    fill_in 'Password', with: admin.password
+    click_button "Log In"
+
+    click_link "See All Users"
+
+    expect(page).to have_content "Jane Smith"
+    expect(page).to have_content "Jim Smith"
+    expect(page).to have_content "James Smith"
+    expect(page).to have_content "Janet Smith"
+  end
+
+  scenario "by navigating to the /users page" do
+    user = FactoryGirl.create(:user)
+    color = FactoryGirl.create(:color, user: user)
+    FactoryGirl.create(:review, user: user, color: color)
+    FactoryGirl.create(:user, first_name: "Jane")
+    FactoryGirl.create(:user, first_name: "Jim")
+    FactoryGirl.create(:user, first_name: "James")
+    FactoryGirl.create(:user, first_name: "Janet")
+    admin = FactoryGirl.create(:user, role: 'admin')
+
+    visit root_path
+    click_link "Sign In"
+
+    fill_in 'Email', with: admin.email
+    fill_in 'Password', with: admin.password
+    click_button "Log In"
+
+    visit users_path
+
+    expect(page).to have_content "Jane Smith"
+    expect(page).to have_content "Jim Smith"
+    expect(page).to have_content "James Smith"
+    expect(page).to have_content "Janet Smith"
+  end
+end
+
+feature "users cannot see a list of users" do
+  scenario "via a link on the home page" do
+    FactoryGirl.create(:user, first_name: "Jane")
+    FactoryGirl.create(:user, first_name: "Jim")
+    FactoryGirl.create(:user, first_name: "James")
+    FactoryGirl.create(:user, first_name: "Janet")
+    user = FactoryGirl.create(:user)
+    color = FactoryGirl.create(:color, user: user)
+    FactoryGirl.create(:review, user: user, color: color)
+
+    visit root_path
+    click_link "Sign In"
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button "Log In"
+
+    expect(page).to_not have_content "See All Users"
+  end
+
+  scenario "by navigating to the /users page" do
+    FactoryGirl.create(:user, first_name: "Jane")
+    FactoryGirl.create(:user, first_name: "Jim")
+    FactoryGirl.create(:user, first_name: "James")
+    FactoryGirl.create(:user, first_name: "Janet")
+    user = FactoryGirl.create(:user)
+    color = FactoryGirl.create(:color, user: user)
+    FactoryGirl.create(:review, user: user, color: color)
+
+    visit root_path
+    click_link "Sign In"
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button "Log In"
+
+    expect { visit users_path }.to raise_error
+  end
+end
