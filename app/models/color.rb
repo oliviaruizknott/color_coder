14071f9a3299eb4bd81ec5cec40d415fcb99ef41
olivia@ruiz-knott.com <olivia@ruiz-knott.com>
2017-04-27 17:10:37 -0400

    Adding PG search back in

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,5 +1,5 @@
 class Color < ApplicationRecord
-  # include PgSearch
+  include PgSearch
 
   validates :hex_code, presence: true, uniqueness: true
   validates_format_of :hex_code, with: /#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})/, message: ": Please enter a valid hex code, including the starting '#'."
