b8ec3d17589b47040214e98de9a909cf96fefb6c
olivia@ruiz-knott.com <olivia@ruiz-knott.com>
2017-05-01 12:15:17 -0400

    Removed search

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,12 +1,7 @@
 class Color < ApplicationRecord
-  include PgSearch
 
   validates :hex_code, presence: true, uniqueness: true
   validates_format_of :hex_code, with: /#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})/, message: ": Please enter a valid hex code, including the starting '#'."
-
-  pg_search_scope :search_color_only, against: [:hex_code, :nickname]
-
-  scope :search, -> (query) { search_color_only(query) if query.present? }
 
   belongs_to :user
   has_many :reviews
